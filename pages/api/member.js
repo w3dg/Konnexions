010c58c59b1575143029b2638311e0473e1e30d0
Konnexions <121281674+kiit-konnexions@users.noreply.github.com>
2023-06-08 17:56:18 +0530

    First 90 Memebers

--- pages/api/member.js
+++ pages/api/member.js
@@ -8,7 +8,7 @@ const handler = async(req, res) => {
       memberPages {
         heading
         description
-        member {
+        member(first: 90) {
           name
           email
           domain
