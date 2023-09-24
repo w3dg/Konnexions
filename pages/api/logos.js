41ffddb8fc8c54b1f3656b5c8fc2cbc4356be250
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-09-24 13:40:17 +0530

    cors

--- pages/api/logos.js
+++ pages/api/logos.js
@@ -1,7 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { client, gql } from "@/graph";
+import NextCors from "nextjs-cors";
 
-const handler = async(req, res) => {
+const handler = async (req, res) => {
+  await NextCors(req, res, {
+    // Options
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+    origin: "*",
+    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  });
+
   const query = gql`
     query Logo {
       logos {
@@ -18,18 +26,21 @@ const handler = async(req, res) => {
       }
     }
   `;
-  
-  await client.request(query).then((data) => {
-    console.log(data);
-    res.status(200).json({
-      data: data.logos[0],
+
+  await client
+    .request(query)
+    .then((data) => {
+      console.log(data);
+      res.status(200).json({
+        data: data.logos[0],
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
     });
-  }).catch((err) => {
-    console.log(err);
-    res.status(500).json({
-      error: err,
-    });
-  });
-}
+};
 
 export default handler;
